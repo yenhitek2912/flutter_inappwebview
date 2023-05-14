3e410a4514181c47dfee6bd98d5de3423439402b
OS.ITBee Nguyễn Xuân Kiệt <kiet.nguyen@itbeesolutions.vn>
2023-05-14 15:22:28 +0700

    test

--- android/src/main/java/com/pichillilorenzo/flutter_inappwebview/webview/in_app_webview/InAppWebViewClient.java
+++ android/src/main/java/com/pichillilorenzo/flutter_inappwebview/webview/in_app_webview/InAppWebViewClient.java
@@ -113,12 +113,43 @@ public class InAppWebViewClient extends WebViewClient {
 
     @Override
     public boolean shouldOverrideUrlLoading(WebView webView, String url) {
-        InAppWebView inAppWebView = (InAppWebView) webView;
-        if (inAppWebView.customSettings.useShouldOverrideUrlLoading) {
-            onShouldOverrideUrlLoading(inAppWebView, url, "GET", null, true, false, false);
-            return true;
-        }
-        return false;
+        //웹뷰 내 표준창에서 외부앱(통신사 인증앱)을 호출하려면 intent:// URI를 별도로 처리해줘야 합니다.
+			//다음 소스를 적용 해주세요.
+            if (url.startsWith("intent://")) {
+                Intent intent = null;
+                try {
+                    intent = Intent.parseUri(url, Intent.URI_INTENT_SCHEME);
+                    if (intent != null) {
+                        //앱실행
+                        webView.getContext().startActivity(intent);
+                    }
+                } catch (URISyntaxException e) {
+					//URI 문법 오류 시 처리 구간
+
+                } catch (ActivityNotFoundException e) {
+                    String packageName = intent.getPackage();
+                    if (!packageName.equals("")) {
+                        // 앱이 설치되어 있지 않을 경우 구글마켓 이동
+                        webView.getContext().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + packageName)));
+                    }
+                }
+				//return  값을 반드시 true로 해야 합니다.
+				return true;
+
+			} else if (url.startsWith("https://play.google.com/store/apps/details?id=") || url.startsWith("market://details?id=")) {
+				//표준창 내 앱설치하기 버튼 클릭 시 PlayStore 앱으로 연결하기 위한 로직
+                Uri uri = Uri.parse(url);
+                String packageName = uri.getQueryParameter("id");
+                if (packageName != null && !packageName.equals("")) {
+                    // 구글마켓 이동
+                    webView.getContext().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + packageName)));
+                }
+				//return  값을 반드시 true로 해야 합니다.
+                return true;
+            }
+
+			//return  값을 반드시 false로 해야 합니다.
+            return false;
     }
 
     private void allowShouldOverrideUrlLoading(WebView webView, String url,
@@ -155,7 +186,6 @@ public class InAppWebViewClient extends WebViewClient {
                         allowShouldOverrideUrlLoading(webView, url, headers, isForMainFrame);
                         break;
                     case CANCEL:
-                        return true;
                     default:
                         break;
                 }
