db0f5894952ef6ee65c3e9d2b5b28ce777a08b81
OS.ITBee Nguyễn Xuân Kiệt <kiet.nguyen@itbeesolutions.vn>
2023-05-14 14:58:34 +0700

    update shouldOverrideUrlLoading function

--- android/src/main/java/com/pichillilorenzo/flutter_inappwebview/webview/in_app_webview/FlutterWebView.java
+++ android/src/main/java/com/pichillilorenzo/flutter_inappwebview/webview/in_app_webview/FlutterWebView.java
@@ -34,12 +34,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import android.app.Activity;
-import android.content.ActivityNotFoundException;
-import android.content.Intent;
-import android.net.Uri;
-import java.net.URISyntaxException;
-
 public class FlutterWebView implements PlatformWebView {
 
   static final String LOG_TAG = "IAWFlutterWebView";
@@ -167,47 +161,6 @@ public class FlutterWebView implements PlatformWebView {
       }
       webView.setWebChromeClient(new WebChromeClient());
       webView.setWebViewClient(new WebViewClient() {
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-
-			//웹뷰 내 표준창에서 외부앱(통신사 인증앱)을 호출하려면 intent:// URI를 별도로 처리해줘야 합니다.
-			//다음 소스를 적용 해주세요.
-            if (url.startsWith("intent://")) {
-                Intent intent = null;
-                try {
-                    intent = Intent.parseUri(url, Intent.URI_INTENT_SCHEME);
-                    if (intent != null) {
-                        //앱실행
-                        startActivity(intent);
-                    }
-                } catch (URISyntaxException e) {
-					//URI 문법 오류 시 처리 구간
-
-                } catch (ActivityNotFoundException e) {
-                    String packageName = intent.getPackage();
-                    if (!packageName.equals("")) {
-                        // 앱이 설치되어 있지 않을 경우 구글마켓 이동
-                        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + packageName)));
-                    }
-                }
-				//return  값을 반드시 true로 해야 합니다.
-				return true;
-
-			} else if (url.startsWith("https://play.google.com/store/apps/details?id=") || url.startsWith("market://details?id=")) {
-				//표준창 내 앱설치하기 버튼 클릭 시 PlayStore 앱으로 연결하기 위한 로직
-                Uri uri = Uri.parse(url);
-                String packageName = uri.getQueryParameter("id");
-                if (packageName != null && !packageName.equals("")) {
-                    // 구글마켓 이동
-                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + packageName)));
-                }
-				//return  값을 반드시 true로 해야 합니다.
-                return true;
-            }
-
-			//return  값을 반드시 false로 해야 합니다.
-            return false;
-        }
         @Override
         public void onPageFinished(WebView view, String url) {
           if (webView.inAppWebViewRenderProcessClient != null) {
